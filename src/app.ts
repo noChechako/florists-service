41142a236e08b18c45689703afed1c6f139978ac
Eugene Habinski <y.gabinsky@redmadrobot.com>
2022-10-06 08:28:19 +0300

    add app class and fix index.ts

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import {errorMiddleware} from './middlewares/error-handler';
+import userRouter from './controllers/user.controller';
+import authRouter from './controllers/auth.controller';
+import {connectDatabase} from './repositories/connect.database';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from '../swagger.json';
+
+class App {
+    public server;
+
+    constructor() {
+        this.server = express();
+        this.persistence();
+        this.middlewares();
+        this.routes();
+        this.openAPI();
+    }
+
+    private middlewares() {
+        this.server.use(express.json());
+        this.server.use(express.urlencoded({extended: true}));
+        this.server.use(errorMiddleware);
+    }
+
+    private routes() {
+        this.server.use('/users', userRouter);
+        this.server.use('/auth', authRouter);
+    }
+
+    private persistence() {
+        connectDatabase();
+    }
+
+    private openAPI() {
+        this.server.use(
+            '/api-docs',
+            swaggerUi.serve,
+            swaggerUi.setup(swaggerJsdoc)
+        );
+    }
+}
+
+export default new App().server;
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,6 @@
-import express from 'express';
-import {connectDatabase} from './repositories/connect.database';
-import userRouter from './controllers/user.controller';
-import authRouter from './controllers/auth.controller';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from '../swagger.json';
-import {errorMiddleware} from './middlewares/error-handler';
-import * as dotenv from 'dotenv';
+import app from './app';
+import {logger} from './utils/logger';
 
-dotenv.config();
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-connectDatabase();
-
-app.use('/users', userRouter);
-app.use('/auth', authRouter);
-
-app.use(errorMiddleware);
-
-app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerJsdoc)
-);
-
-app.listen(process.env.EXTERNAL_PORT || 3000, () => console.log(`Server up at http://localhost:${process.env.EXTERNAL_PORT || 3000}`));
+app.listen(process.env.EXTERNAL_PORT || 3000, () => {
+    logger.info(`Server listening on port ${process.env.EXTERNAL_PORT || 3000}`);
+});
